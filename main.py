ecc80a47b52e02d44dbadbf6ce052b6d44fe2283
Aly <alyasyly8668@gmail.com>
2026-07-14 02:26:56 +0330

    add password generator section

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import tkinter as tk
 import csv
 import tkinter.messagebox as messagebox
-
+import random
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+#Password Generator Project
 
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+def generate_password():
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    password_list.extend(random.choice(letters) for _ in range(nr_letters))
+    password_list.extend(random.choice(symbols) for _ in range(nr_symbols))
+    password_list.extend(random.choice(numbers) for _ in range(nr_numbers))
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.delete(0, tk.END)
+    password_entry.insert(0, password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -82,7 +104,7 @@ password_entry = tk.Entry(width=21)
 password_entry.grid(row=3, column=1, sticky="EW")
 
 # Buttons
-generate_password_button = tk.Button(text='Generate Password')
+generate_password_button = tk.Button(text='Generate Password', command=generate_password)
 generate_password_button.grid(row=3, column=2, sticky="EW")
 
 add_button = tk.Button(text='Add', width=36,command=save_password)
